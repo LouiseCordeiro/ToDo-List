e1e712bfe15fa0a42045e279b0c6c67f71324d8f
LouiseCordeiro <lolovieim@gmail.com>
2022-08-12 13:38:11 -0300

    initial commit

---
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.tutorials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tutorials.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        var todoList = mutableListOf(
+            Todo("Follow AndroidDevs", false),
+            Todo("Learn about RecyclerView", true),
+            Todo("Water my plants", false)
+        )
+
+        val adapter = TodoAdapter(todoList)
+        binding.rvTodos.adapter = adapter
+        binding.rvTodos.layoutManager = LinearLayoutManager(this)
+
+        binding.btnAddTodo.setOnClickListener{
+            val title = binding.etTodo.text.toString()
+            val todo = Todo(title, false)
+            todoList.add(todo)
+            adapter.notifyItemInserted(todoList.size - 1)
+        }
+    }
+}
